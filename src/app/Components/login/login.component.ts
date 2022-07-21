2aad4badb70ced827d1e6c850b390737b95faacd
Affan Hakak <affanshowkat@gmail.com>
2022-07-21 16:31:19 +0530

    added signup and login pages

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  public loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
   }
 
+  login() {
+    this.http
+      .post<any>('http://localhost:3000/login', this.loginForm.value)
+      .subscribe(
+        (res) => {
+          this.loginForm.reset();
+          this.router.navigate(['restaurant']);
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
 }
